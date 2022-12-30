9c00b74a9dce9e323a27a109994bd7579524112d
Eytins <eytins@foxmail.com>
2022-12-30 00:48:08 +0000

    Draw graph and add Dummy Regressor

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib
 from pandas import DataFrame
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso
+from sklearn.linear_model import Ridge
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
+from sklearn.dummy import DummyRegressor
 
 # Data details: listings has totally 75 columns.
 
@@ -21,6 +26,13 @@ def read_data() -> DataFrame:
     return res
 
 
+def draw_origin_data(y):
+    plt.hist(y, bins=50)
+    plt.xlabel('Score')
+    plt.ylabel('Times')
+    plt.show()
+
+
 def pre_process():
     values_origin = read_data()
     values_without_na = values_origin.dropna()
@@ -28,6 +40,7 @@ def pre_process():
     y = values_without_na.iloc[:, -1]
 
     # Data check:
+    print('Abnormal value check:')
     max_x = 0
     min_x = 0
     max_y = 0
@@ -44,12 +57,14 @@ def pre_process():
             max_y = each
         if each < min_y:
             min_y = each
-    print("max_x:", max_x)
-    print("min_x:", min_x)
-    print("max_y:", max_y)
-    print("min_y:", min_y)
-    # -----------
+    print('max_x:', max_x)
+    print('min_x:', min_x)
+    print('max_y:', max_y)
+    print('min_y:', min_y)
+    print('Check done')
+    print()
 
+    # Polynomialize data
     poly = PolynomialFeatures(degree=2)
     poly.fit(x)
     x_poly = poly.transform(x)
@@ -57,19 +72,37 @@ def pre_process():
     return x, y
 
 
-def linear_model(x, y) -> LinearRegression:
+def linear_model(x_train, y_train, x_test, y_test) -> LinearRegression:
     model = LinearRegression()
-    model.fit(x, y)
+    model.fit(x_train, y_train)
+    print('Linear Regression:')
+    print('Slopes:', model.coef_)
+    print('Intercept:', model.intercept_)
+    print('Accuracy:', model.score(x_test, y_test))
+    print()
+    return model
+
+
+def lasso_model(x_train, y_train, x_test, y_test) -> Lasso:
+    model = Lasso()
+    model.fit(x_train, y_train)
+    print('Lasso Regression:')
+    print('Slopes:', model.coef_)
+    print('Intercept:', model.intercept_)
+    print('Accuracy:', model.score(x_test, y_test))
+    print()
     return model
 
 
 def main():
     x, y = pre_process()
+    # draw_origin_data(y)
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.1)
-    model = linear_model(x_train, y_train)
-    print('Coef:', model.coef_)
-    print('Intercept:', model.intercept_)
-    print('Accuracy:', model.score(x_test, y_test))
+    linear_model(x_train, y_train, x_test, y_test)
+
+    dummy = DummyRegressor(strategy='median')
+    dummy.fit(x_train, y_train)
+    print('Baseline score: ', dummy.score(x_test, y_test))
 
 
 if __name__ == '__main__':
